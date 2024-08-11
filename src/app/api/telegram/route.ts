12619a58e315d949df956378a4b1dad606654950
Minhaz Fardin <minhazfardin25@gmail.com>
2024-08-11 17:59:10 +0600

    telegram bot

---
+++ src/app/api/telegram/route.ts
@@ -0,0 +1,27 @@
+import TelegramBot from "node-telegram-bot-api";
+import { NextResponse } from "next/server";
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN!);
+
+export async function POST(request: any) {
+    try {
+         const body = await request.json();
+         if (body.message && body.message.text === '/start') {
+            const chatId = body.message.chat.id;
+            const url = 'https://hamster-key-generator-h75nrnm6t-fardins-projects-caa8af9b.vercel.app';
+      
+            await bot.sendMessage(chatId, 'Click the button below to start:', {
+              reply_markup: {
+                inline_keyboard: [
+                  [{ text: 'Start', url: url }],
+                ],
+              },
+            });
+          }
+
+
+      return NextResponse.json({message: 'success'},{status: 201})
+    } catch (error) {
+       return NextResponse.json({message: error} ,{status: 400})
+    }
+}
